123b78daedb5f8aee24b384b07fe6b22552f4188
stepan <sssprogramer@gmail.com>
2017-12-12 10:22:09 +0300

    fix migration

--- db/migrate/20171212070748_change_year.rb
+++ db/migrate/20171212070748_change_year.rb
@@ -1,5 +1,5 @@
 class ChangeYear < ActiveRecord::Migration[5.0]
   def change
-    change_column :books, :year, :integer
+    change_column :books, :year, :integer, 'integer USING CAST(year AS integer)'
   end
 end
